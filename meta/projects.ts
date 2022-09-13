f4a9b7e1f00abafc324d231aed22b95f5d727071
zoo <csbt1033@gmail.com>
2022-09-13 10:46:22 +0900

    good

--- meta/projects.ts
+++ meta/projects.ts
@@ -1,5 +1,6 @@
 interface Project {
   title: string;
+  intro: string;
   description: string;
   stacks: string[];
   url: string;
@@ -8,38 +9,19 @@ interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Movie App",
-    description: "오픈 API를 활용한 영화정보 사이트",
+    title: "무비 앱",
+    intro: `오픈 API로 만든 영화정보 사이트.`,
+    description: `오픈 API로 만든 영화정보 사이트.`,
     stacks: ["react", "recoil", "react-query", "react-hook-form"],
     url: "https://zzoodev.github.io/movieApp/",
     image: "images/projects/movieApp.png",
   },
   {
-    title: "Movie App",
-    description: "오픈 API를 활용한 영화정보 사이트",
-    stacks: ["react", "recoil", "react-query", "react-hook-form"],
-    url: "https://zzoodev.github.io/movieApp/",
-    image: "images/projects/movieApp.png",
-  },
-  {
-    title: "Movie App",
-    description: "오픈 API를 활용한 영화정보 사이트",
-    stacks: ["react", "recoil", "react-query", "react-hook-form"],
-    url: "https://zzoodev.github.io/movieApp/",
-    image: "images/projects/movieApp.png",
-  },
-  {
-    title: "Movie App",
-    description: "오픈 API를 활용한 영화정보 사이트",
-    stacks: ["react", "recoil", "react-query", "react-hook-form"],
-    url: "https://zzoodev.github.io/movieApp/",
-    image: "images/projects/movieApp.png",
-  },
-  {
-    title: "Movie App",
-    description: "오픈 API를 활용한 영화정보 사이트",
-    stacks: ["react", "recoil", "react-query", "react-hook-form"],
-    url: "https://zzoodev.github.io/movieApp/",
-    image: "images/projects/movieApp.png",
+    title: "땅근 마켓",
+    intro: "NextJs로 만든 미니 풀스택 앱",
+    description: "서버사이드 렌더링, 정적사이트 렌더링",
+    stacks: ["react", "swr", "planet-scale"],
+    url: "https://carrot-market-rho.vercel.app/",
+    image: "images/projects/carrot-market.png",
   },
 ];

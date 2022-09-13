dcc1754cca13f8f57b4f8ac8b648665cd4395eb1
zoo <csbt1033@gmail.com>
2022-09-13 11:39:16 +0900

    ss

--- pages/components/post.tsx
+++ pages/components/post.tsx
@@ -16,22 +16,22 @@ interface PostProps {
 
 const Post: NextPage<PostProps> = ({ post }) => {
   return (
-    <Link href={`/blog/${post.category}/${post.subCategory}/${post.slug}`}>
+    <Link href={`/blog/${post?.category}/${post?.subCategory}/${post?.slug}`}>
       <div className="flex justify-between items-center w-full h-32 py-4 px-2 hover:bg-zinc-300 dark:hover:bg-zinc-800">
         <div>
           <h2 className="font-extrabold text-xl cursor-pointer">
-            {post.title}
+            {post?.title}
           </h2>
           <p className="font-thin text-xs mt-3 cursor-pointer">
-            {post.description}
+            {post?.description}
           </p>
           <span className="font-thin text-xs text-zinc-500">
-            {post.date?.slice(0, 10)}
+            {post?.date?.slice(0, 10)}
           </span>
         </div>
-        {post.thumnail ? (
+        {post?.thumnail ? (
           <img
-            src={post.thumnail}
+            src={post?.thumnail}
             className="h-full aspect-square rounded-md object-cover"
             alt=""
           />

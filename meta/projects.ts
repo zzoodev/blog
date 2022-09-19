8d9b320cc2722df51263f3c8fa18bdf974e67f7d
joyboy10 <csbt1033@gmail.com>
2022-09-20 01:45:25 +0900

    meta updated

--- meta/projects.ts
+++ meta/projects.ts
@@ -12,7 +12,7 @@ export const projects: Project[] = [
     title: "K-PUB",
     intro: "전자책 플랫폼",
     description:
-      "회사에서 리액트 경험자가 저밖에 없었고 SPA로 프론트엔드 개발이 필요하다고해서 프로젝트 리더를 맡아 이끌었습니다.",
+      "회사에서 R&D 사업으로 플랫폼 사이트를 만드는데 리액트 경험자가 저밖에 없어 프론트엔드 개발을 도맡아 했습니다.",
     stacks: [
       "React",
       "Styled-component",
@@ -34,8 +34,8 @@ export const projects: Project[] = [
   },
   {
     title: "무비 앱",
-    intro: `오픈 API로 만든 영화정보 사이트.`,
-    description: `JS 내장 함수인 fetch 함수를 통해 API 통신에 대해 배웠고 서버에서 받은 데이터를 화면에 뿌려주는 작업에 능숙해졌습니다.`,
+    intro: `오픈 API && 리액트로 만든 영화정보 사이트.`,
+    description: `JS 내장 함수인 fetch 함수를 통해 API 통신에 대해 배웠고 React를 활용한 SPA 작업에 능숙해졌습니다.`,
     stacks: ["react", "recoil", "react-query", "react-hook-form"],
     url: "https://zzoodev.github.io/movieApp/",
     image: "images/projects/movieApp.png",
@@ -43,7 +43,7 @@ export const projects: Project[] = [
 
   {
     title: "All T",
-    intro: "Sass로 만든 정적 웹사이트",
+    intro: "Vanilla JS && Sass로 만든 정적 웹사이트",
     description: `자바스크립트로 DOM을 조작하는 것에 익숙해졌습니다. Scss를 활용해 CSS Preprocessor를 경험했습니다.`,
     stacks: ["html", "scss", "javascript"],
     url: "https://zzoodev.github.io/allt/",
